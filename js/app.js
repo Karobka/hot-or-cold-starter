56afdb6c4ab9cebcb73f51032f3c9efb9db58d71
Tim Wright <mithgirw@gmail.com>
2016-06-14 11:37:01 -0600

    Outline for the app

--- js/app.js
+++ js/app.js
@@ -11,6 +11,29 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+/*
+
+Generate random whole secretNumber on page load.
+
+When user clicks #guessButton:
+	Check for empty input.--not needed.
+	Convert userGuess to an intiger.
+	if "userGuess" is a number already added to "guessBox" list: alert"You already guessed that number".
+		else take "userGuess" append as new list item to "guessBox"
+			and append #count text incrementally as user submits guesses.
+	if "userGuess" is exactly equal to #secretNumber update #feedback with you won message.
+		Else if "userGuess" is greater or less than "secretNumber" by 5 update "feedback" with "super hot" message.
+		Else if "userGuess" is greater or less than "secretNumber" by 10 update "feedback" with "hot" message.
+		Else if "userGuess" is greater or less than "secretNumber" by 15 update "feedback" with "warm" message.
+		Else if "userGuess" is greater or less than "secretNumber" by 20 update "feedback" with "luke warm" message.
+		Else if "userGuess" is greater or less than "secretNumber" by 25 update "feedback" with "cold" message.
+		Else if "userGuess" is greater or less than "secretNumber" by 30 update "feedback" with "super cold" message.
+		Else update "feedback" with "completely frozen" message.
+When user clicks .new
+	Reset #feedback to default guess message.
+	Remove user guess li's from .guessBox
+	Reset #count to zero.
+*/
 
 });
 
